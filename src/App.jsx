2fb1ff41aa6796eebf64197d17c5534141876d0f
Abdirahman Nur <myabdi4@gmail.com>
2024-12-21 06:15:25 -0600

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import Blog from "./pages/Blogs/Blog";
 import Home from "./pages/Home/Home";
 import { Routes, Route } from "react-router-dom";
 import Projects from "./pages/Projects/Projects";
-import BlogContent from "./components/BlogContent/BlogContent";
-// import Layout from "./Layout/Layout";
 
 function App() {
   return (
@@ -12,7 +10,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/blogs" element={<Blog />} />
         <Route path="/projects" element={<Projects />} />
-        <Route path="/blog/blogcontent/:id" element={<BlogContent />} />
       </Routes>
     </>
   );
--- src/components/BlogContent/BlogContent.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BlogContent = () => {
-  return <div className="blogcontent">hey</div>;
-};
-
-export default BlogContent;
