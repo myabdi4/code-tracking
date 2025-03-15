7fa568874619bb53e84a033de741f5830eded6d9
Abdirahman Nur <myabdi4@gmail.com>
2025-03-14 22:31:51 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/blogs" element={<Blog />} />
+        {/* <Route path="/blogs" element={<Blog />} /> */}
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </>
